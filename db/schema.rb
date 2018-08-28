4382c6eaf71e7e6d554af1c5eb411bdbb0fcfa79
gusarov2k <motoface@yandex.ru>
2018-08-28 23:52:26 +0300

    bundle exec rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180828204758) do
+ActiveRecord::Schema.define(version: 20180828204824) do
 
   create_table "assemblies", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20180828204758) do
 
   add_index "assemblies_parts", ["assembly_id"], name: "index_assemblies_parts_on_assembly_id"
   add_index "assemblies_parts", ["part_id"], name: "index_assemblies_parts_on_part_id"
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string   "part_number"
