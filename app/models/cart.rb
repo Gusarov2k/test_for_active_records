a328608d67b80970006910c1c460f866636ff77b
gusarov2k <motoface@yandex.ru>
2018-08-29 00:59:17 +0300

    add link in model cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,8 @@ class Cart < ActiveRecord::Base
 	attr_accessible :user_id
 
 	belongs_to 			:user
-	
+	has_and_belongs_to_many :items
+
 	validates 			:user, presence: true
 	validates 			:user_id, uniqueness: true
 end
