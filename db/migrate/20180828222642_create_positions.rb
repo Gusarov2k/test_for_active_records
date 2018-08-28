e3b8cff5dcbe0bf53632152c871931542142bac4
gusarov2k <motoface@yandex.ru>
2018-08-29 01:29:51 +0300

    add columns in migration  positions

--- db/migrate/20180828222642_create_positions.rb
+++ db/migrate/20180828222642_create_positions.rb
@@ -2,7 +2,13 @@ class CreatePositions < ActiveRecord::Migration
   def change
     create_table :positions do |t|
 
-      t.timestamps null: false
-    end
+      t.references 	:cart
+			t.references 	:item
+			t.integer 		:quantity, default: 1
+			t.timestamps
+			end
+			
+			add_index :positions, [:item_id, :cart_id]
+			add_index :positions, [:cart_id, :item_id]
   end
 end
