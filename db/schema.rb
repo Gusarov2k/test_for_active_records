2ff96a5cbd456ba7c56bcb3b33e6eedef184df40
gusarov2k <motoface@yandex.ru>
2018-08-28 23:51:53 +0300

    add code in table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180825095129) do
+ActiveRecord::Schema.define(version: 20180828204758) do
 
   create_table "assemblies", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 20180825095129) do
     t.string   "part_number"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
